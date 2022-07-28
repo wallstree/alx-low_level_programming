b36909744868a8efa96744f390f44faf2901f060
wallstree <ianryol78@gmail.com>
2022-07-28 15:33:37 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,12 +19,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
 
 	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
 
 	if (n < j)
 		j = n;
@@ -42,8 +38,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		arr[co] = s2[co_2];
 		co++;
 	}
-	co++;
 	arr[co] = '\0';
-
 	return (arr);
 }
